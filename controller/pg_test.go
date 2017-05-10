e572aead9d6c1164f78bc2fc7f8107922e1d9ab8
altman <sunhui@datahunter.cn>
2017-05-10 14:39:08 +0800

    init

---
+++ controller/pg_test.go
@@ -0,0 +1,27 @@
+package controller
+
+import (
+	. "datahunter.cn/util"
+	//. "github.com/Shopify/sarama"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"testing"
+	"time"
+)
+
+func Test_Pg(t *testing.T) {
+	db, err := gorm.Open("postgres", "host=localhost user=dh dbname=dh sslmode=disable password=")
+	db.SingularTable(true)
+	defer db.Close()
+	Error(err)
+
+	db.Exec(`drop stream s_demo`)
+	db.Exec(`create stream s_demo (id int, name text, birth timestamp, price float)`)
+	db.Exec(`create continuous view v_demo as select count(*),birth from s_demo group by birth`)
+
+	for i := 0; i < 10; i++ {
+		name := JoinStr("name", i)
+		db.Exec(`insert into s_demo values (?,?,?,?)`, i, name, time.Now(), float32(i))
+	}
+
+}
