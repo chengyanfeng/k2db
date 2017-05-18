3bb2947a598acd09cd85e32b4fd19cc2e32fd703
altman <sunhui@datahunter.cn>
2017-05-18 14:10:18 +0800

    update

--- util/parser.go
+++ util/parser.go
@@ -1,7 +1,7 @@
 package util
 
 import (
-	"github.com/chanxuehong/time"
+	"net/url"
 	"strings"
 )
 
@@ -16,8 +16,14 @@ func (this *LogParser) Parse(msg string) *P {
 	wait := false
 	for _, v := range tmp {
 		if StartsWith(v, `"`) || StartsWith(v, `[`) {
-			wait = true
-			token = v[1:]
+			if EndsWith(v, `"`) {
+				token = Replace(v, []string{`"`}, "")
+				seg = append(seg, token)
+			} else {
+				wait = true
+				token = v[1:]
+			}
+
 		} else {
 			if wait {
 				if EndsWith(v, `"`) || EndsWith(v, `]`) {
@@ -35,16 +41,56 @@ func (this *LogParser) Parse(msg string) *P {
 		}
 
 	}
-	//Debug(JsonEncode(seg))
+	//Debug(len(seg), JsonEncode(seg))
 	p := P{}
-	p["ct"], _ = ToTime(ToString(seg[0]))
-	p["time"] = ToFloat(seg[1])
-	p["client"] = seg[2]
-	p["code"] = seg[3]
+	p["ct"], _ = ToTime(ToString(seg[0])) // 服务器时间戳
+	p["time"] = ToFloat(seg[1])           // 会话持续时间
+	p["client"] = seg[2]                  // 客户端IP
+	p["code"] = seg[3]                    // HTTP返回值
+	p["auth"] = seg[4]                    // 鉴权成功失败
+	p["up"] = seg[5]                      // 上行字节
+	p["down"] = seg[6]                    // 下行字节
+	p["method"] = seg[7]                  // 请求方法（GET POST）
+	p["url"] = seg[8]                     // URL
+	this.ParseUrl(p)                      // URL
+	p["refer"] = seg[9]                   // httpreferer
+	p["ua"] = seg[10]                     // useragent
+	p["hit"] = seg[11]                    // 命中
+	p["rrange"] = seg[12]                 // 请求range
+	p["frange"] = seg[13]                 // 返回range
 	return &p
 }
 
-func (this *LogParser) ParseTime(str string) time.Time {
-	// todo
-	return time.Now()
+func (this *LogParser) ParseUrl(p P) {
+	u, err := url.Parse(ToString(p["url"]))
+	if err != nil {
+		Error(err)
+		return
+	}
+	m, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		Error(err)
+		return
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			Error("ParseUrl", err, m)
+		}
+	}()
+	p["uri"] = u.Path                //
+	p["vkey"] = m["vkey"][0]         //
+	p["locid"] = m["locid"][0]       //
+	p["size"] = m["size"][0]         //
+	p["userid"] = m["userid"][0]     //
+	p["ocid"] = m["ocid"][0]         //
+	p["userip"] = m["userip"][0]     //
+	p["spid"] = m["spid"][0]         //
+	p["pid"] = m["pid"][0]           //
+	p["portalid"] = m["portalid"][0] //
+	p["spip"] = m["spip"][0]         //
+	p["spport"] = m["spport"][0]     //
+	p["sdtfrom"] = m["sdtfrom"][0]   //
+	p["tradeid"] = m["tradeid"][0]   //
+	p["lsttm"] = m["lsttm"][0]       //
+	delete(p, "url")
 }
