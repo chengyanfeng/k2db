5c65c9e0fc1577d0b525607b5828a6d58dd614d4
altman <sunhui@datahunter.cn>
2017-05-20 11:45:42 +0800

    update

--- util/parser_test.go
+++ util/parser_test.go
@@ -9,3 +9,11 @@ func TestLogParser_Parse(t *testing.T) {
 	p := parser.Parse(msg)
 	Debug(JsonEncode(p))
 }
+
+func BenchmarkLogParser_Parse(b *testing.B) {
+	msg := ReadFile("sample.txt")
+	for i := 0; i < b.N; i++ {
+		parser := LogParser{}
+		parser.Parse(msg)
+	}
+}
